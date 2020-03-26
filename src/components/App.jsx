8025d1197a7d2d3de58e72c520383485b34f8b52
Michael Skeen <mmskeen@gmail.com>
2020-03-26 13:06:47 -0600

    finished a form with React hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [submittedName, setSubmittedName] = useState("");
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    setSubmittedName(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {submittedName}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
